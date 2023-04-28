35d9c9387eb28b676d80c28d7d2088d71354a6ea
Zizhang Ai <zizhangai@Zizhangs-MacBook-Pro.local>
2023-04-28 12:36:56 +0800

    Deploy

--- _next/static/EemELUW20Qan4_AsyBTRI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/514-417d3945afd30a0b.js","static/css/aefa92d966281dcd.css","static/chunks/pages/index-80347cf88674e2ad.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
