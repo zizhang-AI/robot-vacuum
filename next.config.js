933524c30844ab04e090b6626b63637239ac4d67
Zizhang Ai <zizhangai@Zizhangs-MacBook-Pro.local>
2023-04-28 12:36:16 +0800

    :bug: fix deployment

--- next.config.js
+++ next.config.js
@@ -2,10 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: process.env.BASE_PATH || '/',
   experimental: {
     appDir: true,
   },
+}
+
+if (process.env.BASE_PATH) {
+  nextConfig.basePath = process.env.BASE_PATH
 }
 
 module.exports = nextConfig
